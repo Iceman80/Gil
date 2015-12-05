b19ae682255aa3e69a67036763d84447b3e9c0b3
Serhiy Yakovlev <ysa.sergey@gmail.com>
2015-12-05 11:45:45 +0200

    1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,9 @@
+
 public class Test {
     public static void main(String[] args) throws Exception {
         Main test = new Main();
-            test.setUp();
+        test.setUp();
+        test.testWw();
+        test.tearDown();
     }
 }
